f0264d9937e27b02d65153c9fb59c8b0c7d39c1c
3203317 <3203317@qq.com>
2016-03-18 19:50:24 +0800

    haha

--- assets/hello.lua
+++ assets/hello.lua
@@ -2,6 +2,6 @@
 
 local var = ngx.var;
 
-
-
+ngx.say('service_name: ', var.service_name);
+ngx.say('</br>');
 ngx.say('tenant_name: ', var.tenant_name);
--- assets/portal.lua
+++
@@ -1,7 +0,0 @@
------注释
-
-local var = ngx.var;
-
-
-ngx.say('门户');
-ngx.say('tenant_name: ', var.tenant_name);
