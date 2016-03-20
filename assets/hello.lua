4cba08a1c7224a2adc50fb3e3c3281350925fecc
3203317 <3203317@qq.com>
2016-03-20 16:51:52 +0800

    haha

--- assets/hello.lua
+++ assets/hello.lua
@@ -1,7 +1,25 @@
------注释
+-- Copyright (C) Xin Huang (3203317@qq.com), http://www.foreworld.net.
 
 local var = ngx.var;
 
 ngx.say('service_name: ', var.service_name);
 ngx.say('</br>');
 ngx.say('tenant_name: ', var.tenant_name);
+ngx.say('</br>');
+
+
+ngx.say('-----------');
+ngx.say('</br>');
+
+local tenant = ngx.shared.tenant;
+
+ngx.say(tenant:get('alias_test001'));
+ngx.say('</br>');
+ngx.say(tenant:get('upstream_portal_test002'));
+ngx.say('</br>');
+
+if nil == tenant:get('alias_test003') then
+    ngx.say('no');
+else
+    ngx.say('yes');
+end
--- assets/init.lua
+++ assets/init.lua
@@ -1,1 +1,14 @@
 -- Copyright (C) Xin Huang (3203317@qq.com), http://www.foreworld.net.
+
+local cjson = require 'cjson';
+
+local file = assert(io.open('/root/my/git/3203317/ss/assets/tenant.json', 'r'));
+local data = cjson.decode(file:read('*all'));
+file:close();
+
+local tenant = ngx.shared.tenant;
+
+for key, value in pairs(data) do
+    tenant:set(key, value);
+end
+
